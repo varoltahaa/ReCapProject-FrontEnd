3219673cfe8ce9e2a4de3775ae77248e9da7582e
Taha Varol <tahavarol98@hotmail.com>
2021-03-14 18:49:58 +0300

    14 Mart 2021

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from 'src/app/models/car';
+import { HttpClient } from '@angular/common/http'
+import { CarResponseModel } from 'src/app/models/carResponseModel';
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'app-car',
@@ -7,16 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
-  car1:any={id:1,brandId:1,colorId:2,modelYear:2015,dailyPrice:200,description:"Dieasel"}
-  car2:any={id:2,brandId:2,colorId:4,modelYear:2015,dailyPrice:200,description:"Otomatik"}
-  car3:any={id:3,brandId:3,colorId:6,modelYear:2015,dailyPrice:200,description:"Benzinli"}
-  car4:any={id:4,brandId:4,colorId:8,modelYear:2015,dailyPrice:200,description:"Son Model"}
-  car5:any={id:5,brandId:5,colorId:10,modelYear:2015,dailyPrice:200,description:"Eski Model"}
-
-  cars = [this.car1,this.car2,this.car3,this.car4,this.car5];
-  constructor() { }
+  cars:Car[] = [];
+  dataLoaded = false;
+  constructor(private carService:CarService) { }
 
   ngOnInit(): void {
+    this.getCars();
+
+  }
+
+  getCars(){
+    this.carService.getCars().subscribe(response => {this.cars = response.data, this.dataLoaded = true});
   }
 
 }
---
+++ src/app/models/rental.ts
@@ -0,0 +1,8 @@
+export interface Rental{
+    id:number;
+    carName:string;
+    firstName:string;
+    lastName:string;
+    rentDate:Date;
+    returnDate:Date;
+}
