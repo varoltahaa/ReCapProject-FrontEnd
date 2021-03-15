ccdf6a335ed4409ff4bd887fd1ad4b0d8aec09b3
Taha Varol <tahavarol98@hotmail.com>
2021-03-15 21:46:12 +0300

    15 Mart 2021

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/car';
 import { HttpClient } from '@angular/common/http'
-import { CarResponseModel } from 'src/app/models/carResponseModel';
 import { CarService } from 'src/app/services/car.service';
+import { ActivatedRoute } from '@angular/router';
+import { parseI18nMeta } from '@angular/compiler/src/render3/view/i18n/meta';
 
 @Component({
   selector: 'app-car',
@@ -13,15 +14,31 @@ export class CarComponent implements OnInit {
 
   cars:Car[] = [];
   dataLoaded = false;
-  constructor(private carService:CarService) { }
+  constructor(private carService:CarService, private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getCars();
-
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["brandId"]){
+        this.getCarsByBrand(params["brandId"])}
+      else if(params["colorId"]){
+        this.getCarsByColor(params["colorId"])
+      }  
+      else{
+        this.getCars();
+      }
+    })
   }
 
   getCars(){
     this.carService.getCars().subscribe(response => {this.cars = response.data, this.dataLoaded = true});
+  }
+
+  getCarsByBrand(brandId:number){
+    this.carService.getCarsByBrand(brandId).subscribe(response => {this.cars = response.data, this.dataLoaded = true});
+  }
+
+  getCarsByColor(colorId:number){
+    this.carService.getCarsByColor(colorId).subscribe(response => {this.cars = response.data, this.dataLoaded = true});
   }
 
 }
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,17 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CarResponseModel } from '../models/carResponseModel';
+import { Car } from '../models/car';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  apiUrl = "https://localhost:44392/api/cars/getcardetail"
+  apiUrl = "https://localhost:44392/api/"
   constructor(private httpClient:HttpClient) { }
 
-  getCars():Observable<CarResponseModel>{
-    return this.httpClient.get<CarResponseModel>(this.apiUrl)
+  getCars():Observable<ListResponseModel<Car>>{
+    let newPath = this.apiUrl + "cars/getcardetail"
+    return this.httpClient.get<ListResponseModel<Car>>(newPath)
+  }
+
+  getCarsByBrand(brandId:number):Observable<ListResponseModel<Car>>{
+    let newPath = this.apiUrl + "cars/getcardetailbybrandid?id="+brandId
+    return this.httpClient.get<ListResponseModel<Car>>(newPath)
+  }
+
+  getCarsByColor(colorId:number):Observable<ListResponseModel<Car>>{
+    let newPath = this.apiUrl + "cars/getcardetailbycolorid?id="+colorId
+    return this.httpClient.get<ListResponseModel<Car>>(newPath)
   }
 }
